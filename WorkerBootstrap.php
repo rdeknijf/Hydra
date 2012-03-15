5de521035555969fa7e41bae8a1d33dc413a0ad4
Rutger de Knijf <rutger@deknijf.com>
2012-03-15 17:43:39 +0100

    Changed autoloader, now it works when run from any working dir

--- WorkerBootstrap.php
+++ WorkerBootstrap.php
@@ -1,9 +1,11 @@
 <?php
 
-//namespace Hydra;
+function hydra_autoload($pClassName) {
+    include(__DIR__ . "/" . $pClassName . ".php");
+}
 
-spl_autoload_register();
+spl_autoload_register('hydra_autoload');
 
 $options = getopt('m:t:v:');
 
-$worker = new Hydra\Worker($options['m'],$options['t'],$options['v']);
+$worker = new Hydra\Worker($options['m'], $options['t'], $options['v']);
